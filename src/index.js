78b910c92cec336660d71a34e023c5def3b39b7a
luckyjek <70943889+luckyjek@users.noreply.github.com>
2022-03-01 15:34:51 +0900

    Add youtube service

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app';
+import Youtube from './service/youtube';
 
+const youtube = new Youtube(process.env.REACT_APP_YOUTUBE_API_KEY);
+console.log(youtube);
 ReactDOM.render( <
   React.StrictMode >
-  <
-  App / >
+  < App 
+  youtube={youtube}/ >
   <
   /React.StrictMode>,
   document.getElementById('root')
